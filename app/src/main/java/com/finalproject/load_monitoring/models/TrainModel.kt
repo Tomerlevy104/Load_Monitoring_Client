c79b82f04f1fe0f7e774ac48085e6925c7dc8821
Tomer Levy <tomerlv104@gmail.com>
2026-01-12 23:42:21 +0200

    refactor: Refactored models and added DTO layer

---
+++ app/src/main/java/com/finalproject/load_monitoring/models/TrainModel.kt
@@ -0,0 +1,20 @@
+package com.finalproject.load_monitoring.models
+
+data class TrainModel(
+    val trainID: String, // Train ID
+    val originStation: String, // Origin station
+    val destinationStation: String, // Destination station
+    var departureTime: String, // Departure time
+    var arrivalTime: String, // Arrival time
+    val currentStation: String, // Current station
+    var lastUpdated: String, // Last updated time
+    val carriageList: List<CarriageModel>, // Carriage list
+
+    val originPlatform: Int, // Departure platform
+    val destinationPlatform: Int // Destination platform
+
+
+)
+
+
+
