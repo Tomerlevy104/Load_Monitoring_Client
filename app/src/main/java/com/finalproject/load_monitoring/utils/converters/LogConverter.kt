91a8f185027ebba2741dd6de821211d34a1f438d
Omer Meler <65625281+OmerMel@users.noreply.github.com>
2026-08-01 20:43:38 +0300

    feat: Added LogConverter for mapping between OccupancyLogDTO and OccupancyLogModel

---
+++ app/src/main/java/com/finalproject/load_monitoring/utils/converters/LogConverter.kt
@@ -0,0 +1,32 @@
+package com.finalproject.load_monitoring.utils.converters
+
+import com.finalproject.load_monitoring.dto.OccupancyLogDTO
+import com.finalproject.load_monitoring.models.OccupancyLogModel
+
+class LogConverter {
+    companion object {
+        fun fromDTOToModel(dto: OccupancyLogDTO): OccupancyLogModel {
+            return OccupancyLogModel(
+                logId = dto.logId,
+                carriageId = dto.carriageId,
+                cameraCount = dto.cameraCount,
+                irCount = dto.irCount,
+                calculatedOccupancy = dto.calculatedOccupancy,
+                timestamp = dto.timestamp.toString()
+            )
+        }
+
+        fun fromModelToDTO(
+            model: OccupancyLogModel
+        ): OccupancyLogDTO {
+            return OccupancyLogDTO(
+                logId = model.logId,
+                carriageId = model.carriageId,
+                cameraCount = model.cameraCount,
+                irCount = model.irCount,
+                calculatedOccupancy = model.calculatedOccupancy,
+                timestamp = model.timestamp
+            )
+        }
+    }
+}
